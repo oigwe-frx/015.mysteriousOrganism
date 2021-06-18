322b47eb08ac4cd1cdd962a09540115cda1b2e66
osita-igwe <84817870+osita-igwe@users.noreply.github.com>
2021-06-18 08:20:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,14 +13,18 @@ const mockUpStrand = () => {
   return newStrand
 }
 
-
+// A factory function that has two parameters: The first parameter is number (no two organisms should have the same number). The second parameter is an array of 15 DNA bases. Return an pAequor object that contains properties and methods
 const pAequorFactory = (number, array) => {
   return {
     //Properties
     specimenNum: number,
     dna: array,
 
+
+
     //Methods
+
+    //.mutate() a method responsible for randomly selecting a base in the object’s dna property and changing the current base to a different base.
     mutate() {
       let index = Math.floor(Math.random()*15);
       let baseOptions = ['A','T','C','G'];
@@ -31,6 +35,9 @@ const pAequorFactory = (number, array) => {
       return this.dna
     }, 
 
+
+
+    //.compareDNA() a method that has one parameter, another pAequor object. The behavior of .compareDNA() is to compare the current pAequor‘s .dna with the passed in pAequor‘s .dna and compute how many bases are identical and in the same locations. .compareDNA() does not return anything, but prints a message that states the percentage of DNA the two objects have in common.
     compareDNA(pAequorObj){
       let spec1 = this.dna;
       let spec2 = pAequorObj.dna;
@@ -49,6 +56,9 @@ const pAequorFactory = (number, array) => {
       console.log(`specimen #1 and specimen #2 have ${percentage}% DNA in common`)
     },
 
+
+
+//The method returns true if the object's .dna array contains at least 60% 'C' or 'G' bases. Otherwise returns false. 
 willLikelySurvive(){
       let counter = 0;
 
@@ -64,10 +74,12 @@ willLikelySurvive(){
       if(percentage >= 60) {return true}
       else {return false};
 },
-  }
-}
+  } //end of object
+} //end of function
 
 
+
+//This function is used to provide the research team with the requested number of simulated DNA sequences that can potentially survive in their natural environment. The function has one parameter, number. This number represents the requested number of sequencs. The function will return an array of pAequor objects.
 const viableStrands = (number) => {
     let counter = 1;
     let dnaArray = [];
